ce3bc82d9c93d17eeafcfde4ac22ad384f1f87b7
Jon Stefansson <jonstef1667@gmail.com>
2013-12-15 21:57:42 -0800

    Add a more realistic KeyListener

---
+++ src/main/java/net/jonstef/redis/EventProcessingKeyListener.java
@@ -0,0 +1,64 @@
+package net.jonstef.redis;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.SessionCallback;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import static net.jonstef.redis.Keys.*;
+
+/**
+ * @author Jon Stefansson
+ */
+public class EventProcessingKeyListener implements KeyListener {
+
+	private final StringRedisTemplate redisTemplate;
+	private final Logger logger = LoggerFactory.getLogger(EventProcessingKeyListener.class);
+
+	public EventProcessingKeyListener(StringRedisTemplate redisTemplate) {
+		this.redisTemplate = redisTemplate;
+	}
+
+	@Override
+	public void process(final String key) {
+		logger.info("process(key={})", key);
+		Map<Object, Object> eventData = redisTemplate.boundHashOps(key).entries();
+		List<Object> results = redisTemplate.executePipelined(new SessionCallback() {
+			@Override
+			public Object execute(RedisOperations operations) throws DataAccessException {
+				List<Object> results;
+				if (isSuccess(key)) {
+					operations.multi();
+					operations.opsForList().remove(EVENT_PROCESSING, 0, key);
+					operations.opsForList().rightPush(EVENT_SUCCESS, key);
+					operations.expire(key, 24, TimeUnit.HOURS);
+					results = operations.exec();
+				}
+				else {
+					operations.multi();
+					operations.opsForList().remove(EVENT_PROCESSING, 0, key);
+					operations.opsForList().rightPush(EVENT_FAILED, key);
+					operations.expire(key, 24, TimeUnit.HOURS);
+					results = operations.exec();
+				}
+				return results;
+			}
+		});
+		logger.info("results = {}", results);
+	}
+
+	private boolean isSuccess(String key) {
+		UUID uuid = UUID.fromString(key.substring(6));
+		boolean success = (uuid.getLeastSignificantBits() % 2) == 0;
+		logger.info("isSuccess(key={}) {}", key, success);
+		return success;
+	}
+
+}
